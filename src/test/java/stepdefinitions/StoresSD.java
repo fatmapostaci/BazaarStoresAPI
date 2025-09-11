cb0596762872e096692c501e4062d00bb38857df
fatmapostaci <fatmapostaci@gmail.com>
2025-09-12 01:20:20 +0300

    pom.xml de maven plugin  runnerfatma olarak editlendi

--- src/test/java/stepdefinitions/StoresSD.java
+++ src/test/java/stepdefinitions/StoresSD.java
@@ -21,20 +21,16 @@ public class StoresSD {
 
     public static JsonNode json;
 
-
     @And("user sends POST request to create a store")
     public void userSendsPOSTRequestToCreateAStore() {
-
         json = JsonUtils.readJson("stores\\storesPostBody");
         response = given(spec).body(json).post("/api/stores/create");
-        response.prettyPrint();
 
     }
     @And("user keeps created id")
     public void userKeepsCreatedId() {
         ConfigReader.setProperty( "validStoreId", response.jsonPath().getString("product.id") );
         System.out.println("userKeepsCreatedId=" + ConfigReader.getProperty("validStoreId"));
-
     }
 
     @Then("user verifies status code {int}")
@@ -45,50 +41,42 @@ public class StoresSD {
     @When("user sends GET request to get all stores and keep ids")
     public void userSendsGETRequestToGetAllStoresAndKeepIds() {
         response = given(spec).get("/api/stores/");
-       // response.jsonPath().getList("id");
     }
 
     @Given("user sends get request with store id as {string}")
     public void userSendsGetRequestWithStoreIdAs(String id) {
-
-        System.out.println("userSendsGetRequestWithStoreIdAs = " + ConfigReader.getProperty(id));
-
         response = given(spec).get("/api/stores/" + ConfigReader.getProperty(id));
-        response.prettyPrint();
     }
-
 
     @Given("user sends put request with store id as {string}")
     public void userSendsPutRequestWithStoreIdAs(String id) {
 
         json = JsonUtils.readJson("stores\\storesPUTBody");
         response = given(spec).body(json).put("/api/stores/"+ConfigReader.getProperty(id));
-        response.prettyPrint();
-
     }
-
 
     @Given("user sends delete request with store id as {string}")
     public void userSendsDeleteRequestWithStoreIdAs(String id) {
         response = given(spec).body(json).delete("/api/stores/"+ConfigReader.getProperty(id));
-        response.prettyPrint();
     }
 
     @Given("user deletes {string} key from put json file")
     public void userDeletesAKeyFromPutJsonFile(String key) {
 
+        System.out.println("json.toString() setJsonDeleteKey= " + json.toString());
         JsonUtils.setJsonDeleteKey(json, key);
-        System.out.println("json.asText() = " + json.toString());
+        System.out.println("json.toString() setJsonDeleteKey = " + json.toString());
     }
 
     @Given("user updates {string} key with value as {string} of put json file")
     public void userUpdatesKeyWithValueAsOfPutJsonFile(String key,String value) {
-
         JsonUtils.setJson(json,key,value);
+        System.out.println("json.toString() = " + json.toString());
     }
 
     @And("user renames {string} key as {string} of put json file")
     public void userRenamesKeyAsOfPutJsonFile(String oldKey, String newKey) {
         JsonUtils.setJsonRenameKey(json,oldKey,newKey);
+        System.out.println("json.toString() = " + json.toString());
     }
 }
